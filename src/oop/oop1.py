455a2cb059f5cf707af570bbce9eea2ea6aca8c3
BuildTools <arthurpisakhov1@gmail.com>
2019-11-18 14:32:34 -0500

    oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,38 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: # Base Class
+  def __init__(self, name):
+    self.name = name
+    pass
+
+class FlightVehicle(Vehicle):
+  def __init__(self, name):
+    super().__init__(name)
+    pass
+
+class Starship(FlightVehicle):
+  def __init__(self, name):
+    super().__init__(name)
+    pass
+
+class GroundVehicle(Vehicle):
+  def __init__(self, name):
+    super().__init__(name)
+    pass
+
+class Airplane(FlightVehicle):
+  def __init__(self, name):
+    super().__init__(name)
+    pass
+
+class Car(GroundVehicle):
+  def __init__(self, name):
+    super().__init__(name)
+    pass
+
+class Motorcycle(GroundVehicle):
+  def __init__(self, name):
+    super().__init__(name)
+    pass
